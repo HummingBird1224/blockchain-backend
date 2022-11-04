6a559eff59cbc0fe321f525fe94ea495bcd89be8
OksanaShuhaliia <OksanaShuhaliia@gmail.com>
2022-11-04 12:27:29 -0700

    marketplace-update

---
+++ app/routes/auth.routes.js
@@ -0,0 +1,31 @@
+const { verifySignUp } = require("../middlewares");
+const controller = require("../controllers/auth.controller");
+
+module.exports = function(app) {
+  app.use(function(req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  });
+
+  app.post(
+    "/api/auth/signup",
+    [
+      verifySignUp.checkDuplicateUsernameOrEmail,
+      verifySignUp.checkRolesExisted
+    ],
+    controller.signup
+  );
+
+  app.post("/api/auth/signin", controller.signin);
+
+  app.post("/api/auth/signin_admin", controller.signin_admin);
+
+  app.post("/api/auth/requestPasswordReset", controller.requestPasswordReset);
+
+  app.post("/api/auth/resetPassword", controller.resetPassword);
+
+  app.post("/api/auth/twofactorVerify", controller.twofactorVerify);
+};
